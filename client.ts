7f4d76762a6151921c21439e4493ebea5fbe61dc
wobsoriano <sorianorobertc@gmail.com>
2023-07-30 20:57:15 -0700

    remove unused types

--- client.ts
+++ client.ts
@@ -7,31 +7,6 @@ type ThumbHash = {
   originalHeight: number
 }
 
-declare module '*.jpg?th' {
-  const th: ThumbHash
-  export default th
-}
-
-declare module '*.jpeg?th' {
-  const th: ThumbHash
-  export default th
-}
-
-declare module '*.png?th' {
-  const th: ThumbHash
-  export default th
-}
-
-declare module '*.webp?th' {
-  const th: ThumbHash
-  export default th
-}
-
-declare module '*.avif?th' {
-  const th: ThumbHash
-  export default th
-}
-
 declare module '*.jpg?thumb' {
   const th: ThumbHash
   export default th
