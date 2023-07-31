d269885a7e56b5b4e77a22e870d2e187d7cdf3a0
wobsoriano <sorianorobertc@gmail.com>
2023-07-30 21:32:40 -0700

    add dev playground

---
+++ dev/index.ts
@@ -0,0 +1,22 @@
+import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
+import Image from './download.jpg?thumb'
+
+const page = `<!DOCTYPE HTML>
+<html lang="en">
+    <body>
+        <h1>Example</h1>
+        <img src="${Image.src}" id="demo" width="${Image.width}" height="${Image.height}">
+
+        <script>
+          const demo = document.getElementById('demo')
+          setTimeout(() => demo.src = "${Image.originalSrc}", 1000)
+        </script>
+    </body>
+</html>`
+
+new Elysia()
+    .use(html())
+    .get('/', () => page)
+    .get('/html', ({ html }) => html(page))
+    .listen(8080)
--- playground.ts
+++
@@ -1,5 +0,0 @@
-import Image from "./download.jpg?thumb"
-// import Image2 from './download.jpg'
-
-// console.log(Image2);
-console.log(Image)
